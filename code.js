d234268b144e6ad8716196424b897429764844eb
Juliana <123640083+ju-c-santos@users.noreply.github.com>
2026-02-19 22:29:26 -0300

    falta só a validação da senha

--- code.js
+++ code.js
@@ -2,8 +2,12 @@ const escolha = document.querySelector('#escolhaUsuario') ///pegar o item seleci
 
 const usuario = document.getElementById('usuario') 
 const senha = document.getElementById('senha')
+const verSenha = document.getElementById('imagem')
+
 
 const forms = document.getElementById('conteudo'); // formulario em que está o conteudo
+
+let views = false
 
 
 escolha.addEventListener("change", (evt) => { //mostrar o item selecionado no console
@@ -11,7 +15,7 @@ escolha.addEventListener("change", (evt) => { //mostrar o item selecionado no co
     const msgError = document.getElementById('userError');
     msgError.style.display = 'none';
 
-    console.log(escolhaLogin);
+    //console.log(escolhaLogin);
 
     if(escolhaLogin === 'email'){
         usuario.placeholder = 'email';
@@ -22,13 +26,13 @@ escolha.addEventListener("change", (evt) => { //mostrar o item selecionado no co
             if (usuario.value.includes('@' && '.com')){
                 msgError.innerHTML = '';
                 msgError.style.display = 'none';
-                console.log('tem o arroba')
+                //console.log('tem o arroba')
 
             } else if (escolhaLogin === 'nickname'){ // é pra nao da erro de validar o nickname
                 msgError.innerHTML = '';
                 msgError.style.display = 'none';
                 usuario.placeholder = 'nickname';
-                console.log('nao é imail')
+                //console.log('nao é imail')
             } else {
                 msgError.style.display = 'block';
                 msgError.innerHTML = 'o e-mail deve possuir <strong>@</strong> e <strong>.com</strong>.'
@@ -42,15 +46,35 @@ escolha.addEventListener("change", (evt) => { //mostrar o item selecionado no co
     usuario.addEventListener('blur', (disfoco) =>{
         msgError.innerHTML = '';
         msgError.style.display = 'none';
-        console.log('desfocado')
+        //console.log('desfocado')
     })
 
 
 })
 
+///viasualizar senha
+verSenha.addEventListener('click', (ver) => {
+
+    if (views == false){
+        verSenha.src = 'img/hide.png'
+        senha.type = 'text';
+        //console.log('da pra ve')
+        views = true;
+
+    } else {
+        verSenha.src = 'img/view.png'
+        senha.type = 'password';
+        //console.log('nao da pra ve')
+        views = false;
+    }
+
+
+})
+
+
 /// senha evento blur
 senha.addEventListener('blur', (senhaV) => {
-    
+   
 
 
     
